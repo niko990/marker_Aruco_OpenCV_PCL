41d4ae7958e22ba80aa2be989452f7db9f4f29ef
niko990 <nico990.lanza@gmail.com>
2013-12-20 15:19:12 +0100

    Avanzamento generale
    Punto della situazione:
    - il vettore di interi IDcomuni contiene in ordine tutti gli ID presenti
    in entrambe le nuvole.
    - dalla riga 491 inizia il nuovo codice con la creazione delle strutture
    necessarie e l'aggiunta delle corrispondenze trovate con stampa a video
    dei dettagli del processo.
    
    La riga cruciale ora è
    SVD.estimateRigidTransformation (&markers_cloudA, &markers_cloudB,
    correspondences, transformation_matrix);
    ho provato diversi modi per chiamarla ma ricevo sempre errore per gli
    argomenti non corretti e sono bloccato...
    
    La funzione dovrebbe scrivere nella matrice transformation_matrix il
    risultato della trasformazione delle due nuvole usando le corrispondenze
    aggiunte precedentemente.
    Poi si applica pcl::transformPointCloud (*markers_cloudA, cloud_out,
    trans); e si ottiene la nuvola cloud_out che è stata trasformata secondo
    questa matrice e la si unisce alla prima, ottenendo l'unione.
    
    Le funzioni sono quelle, ci sono vari modi per calcolare la matrice di
    trasformazione ma cambia solo il metodo di calcolo, il problema è
    proprio che non accetta gli argomenti in input.
    Ora devo andare via, dacci un occhio se riesci perchè è la fine del
    progetto questa...

--- aruco_simple_board.cpp
+++ aruco_simple_board.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-#include <fstream>
 #include <sstream>
-#include <iostream>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <vector>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include "aruco.h"
@@ -17,12 +15,23 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl\common\transforms.h>
+#include <pcl\registration\transformation_estimation_svd.h>
+#include <pcl\registration\transformation_estimation.h>
+#include <pcl\common\transformation_from_correspondences.h>
 
-#define _USE_MATH_DEFINES
 
-using namespace cv;
+//using namespace cv;
 using namespace aruco; 
-using namespace pcl;
+//using namespace pcl;
+
+/*
+struct marker_struct{
+	int id;
+	pcl::PointXYZRGBA v1;
+	pcl::PointXYZRGBA v2;
+	pcl::PointXYZRGBA v3;
+	pcl::PointXYZRGBA v4;
+};*/
 
 /*
 Vector<pcl::PointXYZRGBA> CentersA;
@@ -40,8 +49,9 @@ void viewerOneOff (pcl::visualization::PCLVisualizer& viewer) {
 }*/
 
 // CONVERSIONE DA PCD A IMMAGINE MAT
+
 cv::Mat pcd2img (pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud){
-		cv::Mat test = cv::Mat(Size(cloud->width, cloud->height), CV_8UC3);
+		cv::Mat test = cv::Mat(cv::Size(cloud->width, cloud->height), CV_8UC3);
 		int i, j;
 		for (int k = 0; k < cloud->points.size (); k++) {
 			i= k % cloud->width;
@@ -65,7 +75,7 @@ void getNearestPoint
  
 	// Controllo se p è un NaN o infinito e lo ritorno se è un valore corretto
 	if (!(p.x == std::numeric_limits<float>::quiet_NaN ()) && !(p.x == std::numeric_limits<float>::infinity())){
-		cout << "\n getNearestPoint - Indice punto = " << k << " - " << p << "\n";
+		//cout << "\n getNearestPoint - Indice punto = " << k << " - " << p << "\n";
 
 		// inserisci nella nuvola di marker di output passata il primo punto scelto
 		output_markers_cloud->points[k] = p;
@@ -92,7 +102,7 @@ void getNearestPoint
 		}
 
 		if (!(p.x == std::numeric_limits<float>::quiet_NaN ()) && !(p.x == std::numeric_limits<float>::infinity())){
-			cout << "\n getNearestPoint (seconda scelta) - Indice punto = " << k << " - " << p << "\n";
+			//cout << "\n getNearestPoint (seconda scelta) - Indice punto = " << k << " - " << p << "\n";
 		}
 		else{
 			cout << "Error: non è stato possibile trovare una corrispondenza 3D per il punto (" << i << "," << j << ")\n";
@@ -109,6 +119,182 @@ void getNearestPoint
 }
 
 
+// RICERCA DEL PUNTO 3D NELLA NUVOLA IN INPUT DATE X,Y
+// RETURN EIGEN 3f
+Eigen::Vector3f getNearestPoint3f (pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud, float i, float j) {
+	
+	int k = (cloud->width * (int)(j + 0.5) ) + (int)(i + 0.5);
+	//pcl::PointXYZRGBA p = cloud->points[k];
+	Eigen::Vector3f v (cloud->points[k].x, cloud->points[k].y, cloud->points[k].z);
+	
+	//cout << "ritorno " << v << endl;
+	
+	/*
+	v.x << cloud->points[k].x;
+	v.y << cloud->points[k].y;
+	v.z << cloud->points[k].z;
+	*/
+	/*
+	// Controllo se p è un NaN o infinito e lo ritorno se è un valore corretto
+	if (!(v.x == std::numeric_limits<float>::quiet_NaN ()) && !(v.x == std::numeric_limits<float>::infinity())){
+		//cout << "\n getNearestPoint - Indice punto = " << k << " - " << p << "\n";
+
+		// inserisci nella nuvola di marker di output passata il primo punto scelto
+		//output_markers_cloud->points[k] = p;
+
+		return v;
+	}
+	else {
+		// altrimenti calcolo l'errore commesso dall'approssimazione e cerco un punto nell'intorno
+		// di p0 che è più vicino e lo ritorno (controllando che non sia NaN o inf)
+		float di = i - (int)(i + 0.5);
+		float dj = j - (int)(j + 0.5);
+	
+		if ( abs(di) <= abs(dj)){
+			if (di > 0){
+				v.x = cloud->points[k + 1].x;
+				v.y = cloud->points[k + 1].y;
+				v.z = cloud->points[k + 1].z;
+			} else {
+				v.x = cloud->points[k - 1].x;
+				v.y = cloud->points[k - 1].y;
+				v.z = cloud->points[k - 1].z;
+			}
+		}
+		else {
+			if (dj > 0){
+				v.x = cloud->points[k + cloud->width].x;
+				v.y = cloud->points[k + cloud->width].y;
+				v.z = cloud->points[k + cloud->width].z;
+			} else {
+				v.x = cloud->points[k - cloud->width].x;
+				v.y = cloud->points[k - cloud->width].y;
+				v.z = cloud->points[k - cloud->width].z;
+			}
+		}
+
+		if (!(v.x == std::numeric_limits<float>::quiet_NaN ()) && !(v.x == std::numeric_limits<float>::infinity())){
+			//cout << "\n getNearestPoint (seconda scelta) - Indice punto = " << k << " - " << p << "\n";
+		}
+		else{
+			cout << "Error: non è stato possibile trovare una corrispondenza 3D per il punto (" << i << "," << j << ")\n";
+			v.x = -1;
+		}
+
+		// inserisci nella nuvola di marker di output passata il secondo punto scelto
+		//output_markers_cloud->points[k] = p;
+	}
+	*/
+	return v;
+
+}
+
+
+
+pcl::PointXYZRGBA getNearestPoint (pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud, float i, float j) {
+	
+	int k = (cloud->width * (int)(j + 0.5) ) + (int)(i + 0.5);
+	pcl::PointXYZRGBA p = cloud->points[k];
+
+ 
+	// Controllo se p è un NaN o infinito e lo ritorno se è un valore corretto
+	if (!(p.x == std::numeric_limits<float>::quiet_NaN ()) && !(p.x == std::numeric_limits<float>::infinity())){
+		//cout << "\n getNearestPoint - Indice punto = " << k << " - " << p << "\n";
+
+		// inserisci nella nuvola di marker di output passata il primo punto scelto
+		//output_markers_cloud->points[k] = p;
+
+		return p;
+	}
+	else {
+		// altrimenti calcolo l'errore commesso dall'approssimazione e cerco un punto nell'intorno
+		// di p0 che è più vicino e lo ritorno (controllando che non sia NaN o inf)
+		float di = i - (int)(i + 0.5);
+		float dj = j - (int)(j + 0.5);
+	
+		if ( abs(di) <= abs(dj)){
+			if (di > 0)
+				p = cloud->points[k + 1];
+			else
+				p = cloud->points[k - 1];
+		}
+		else {
+			if (dj > 0)
+				p = cloud->points[k + cloud->width];
+			else
+				p = cloud->points[k - cloud->width];
+		}
+
+		if (!(p.x == std::numeric_limits<float>::quiet_NaN ()) && !(p.x == std::numeric_limits<float>::infinity())){
+			//cout << "\n getNearestPoint (seconda scelta) - Indice punto = " << k << " - " << p << "\n";
+		}
+		else{
+			cout << "Error: non è stato possibile trovare una corrispondenza 3D per il punto (" << i << "," << j << ")\n";
+			p.x = -1;
+		}
+
+		// inserisci nella nuvola di marker di output passata il secondo punto scelto
+		//output_markers_cloud->points[k] = p;
+	}
+	
+	return p;
+
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////     ERRORE !!!!!!!!!!!!!!!   FUNZIONE DA SISTEMARE !!!!!!!!!!!!!!!!!!!!!!!!!!!
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Ritorna un vettore di interi contenente gli ID ordinati (crescenti) dei marker persenti in entrambe le nuvole
+/*void ricercaCorrispondenzeID (std::vector<Marker> markerA, std::vector<Marker> markerB, int IDcomuni[]){
+	//std::vector<int> IDcomuni;
+	
+	//IDcomuni->resize(min(markerA.size(), markerB.size()));
+	IDcomuni->resize(min(markerA.size(), markerB.size()));
+	int pos = 0, validID = 0;
+
+	for (int i = 0; i < markerA.size();; i++){
+		for (int j = 0; j <	markerB.size();; j++){
+					
+			if (markerA[i].id != markerB[j].id)
+				continue;
+			
+			//cout << "i = " << i << " j = " << j << " - markerA[i].id = " << markerA[i].id << " marker[j].id = " << markerB[j].id << " * Uguale? "<< (markerA[i].id == markerB[j].id) << endl;
+
+			IDcomuni[pos] = markerA[i].id;
+			pos++;
+			break;
+		}
+	}
+
+	/*
+	for (int i=0;i<IDcomuni.size();i++){
+		if (IDcomuni[i] != 0)
+			validID++;
+		cout << "IDcomuni[" << i << "] = " <<IDcomuni[i]<< endl;
+	}
+
+	cout << "resize a -> " << validID << endl; 
+	
+	IDcomuni->resize(validID);
+
+		for (int i=0;i<IDcomuni->size();i++)
+			cout << "IDcomuni[" << i << "] = " <<IDcomuni[i]<< endl;
+
+	//return IDcomuni;
+}
+*/
+
+
+
+
+
+
+
+
 
 int main(int argc,char **argv) {
 	try	{
@@ -122,9 +308,10 @@ int main(int argc,char **argv) {
 		pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloudB (new pcl::PointCloud<pcl::PointXYZRGBA>);
 		pcl::PointCloud<pcl::PointXYZRGBA>::Ptr markers_cloudA (new pcl::PointCloud<pcl::PointXYZRGBA>);
 		pcl::PointCloud<pcl::PointXYZRGBA>::Ptr markers_cloudB (new pcl::PointCloud<pcl::PointXYZRGBA>);	
-		pcl::PointCloud<pcl::PointXYZRGBA>::Ptr rotatedCloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
 		cv::Mat InImageA, InImageB;
 		pcl::PassThrough<pcl::PointXYZRGBA> pass;
+		//std::vector<int> IDcomuni ;
+		//int IDcomuni[];
 		
 
 		// LETTURA DEGLI ARGOMENTI IN INPUT
@@ -181,32 +368,107 @@ int main(int argc,char **argv) {
 
 
 
-		
+
+
+
+
+
+		/*
+		cout << "RICERCA DELLE CORRISPONDENZE!\n";
+		cout << "#markersA = " << MarkersA.size() << ", #markersB = " << MarkersB.size() << ", IDcomuni.size() = " << IDcomuni.size() << endl;
+		// Vettore contenente tutti gli ID dei marker presenti in entrambe le nuvole
+		int IDcomuni [MarkersA.size()];
+		ricercaCorrispondenzeID (MarkersA, MarkersB, IDcomuni);
+
+
+		cout << "#markersA = " << MarkersA.size() << ", #markersB = " << MarkersB.size() << ", IDcomuni.size() = " << IDcomuni.size() << endl;
+		for (int i = 0; i < IDcomuni.size(); i++)
+		std::cout << "IDcomuni = " << IDcomuni[i] << endl;
+		*/
+
+
+		// RICERCA DELLE COSSIRPONDENZE DEGLI ID NEI VETTORI MarkersA e MarkersB
+		const int IDcomuni_size = 20;
+		int IDcomuni[IDcomuni_size];
+		int pos = 0, validID = 0;
+
+		// IDcomuni inizializzato a tutti 0
+		for (int i=0; i < IDcomuni_size; i++)
+			IDcomuni[i] = 0;
+
+		// Inizio ricerca, gli ID sono ordine crescente
+		for (int i = 0; i < MarkersA.size(); i++){
+			for (int j = 0; j <	MarkersB.size(); j++){
+					
+				if (MarkersA[i].id != MarkersB[j].id)
+					continue;
+			cout << "i = " << i << " j = " << j << " - markerA[i].id = " << MarkersA[i].id << " marker[j].id = " << MarkersB[j].id << " * Uguale? "<< (MarkersA[i].id == MarkersB[j].id) << endl;
+			IDcomuni[pos] = MarkersA[i].id;
+			pos++;
+			break;
+			}
+		}
+
+		//cout << "min (A, B) = " << min(MarkersA.size(), MarkersB.size()) << endl;
+
+		// Tronco il vettore alla lunghezza esatta del totale degli elementi trovati
+		for (int i=0; i < IDcomuni_size ;i++){
+			if (IDcomuni[i] == 0) {
+				IDcomuni[i] = '\0';
+				break;
+			}
+		}
+
+		// stampo il vettore
+		for (int i = 0; IDcomuni[i]; i++)
+			cout << "IDcomuni[" << i << "] = " << IDcomuni[i] <<  endl;
+
+
+
+
+
+
+
+
+
+
 		// Stampa info su immagine A
 		std::cout << "\n*** MARKERS IMMAGINE A ***\n";
 		for(unsigned int i=0;i<MarkersA.size();i++){
-			cout << MarkersA[i].id << endl;
+			//cout << MarkersA[i].id << endl;
+			
+			//markersA_struct[i].id = MarkersA[i].id;
+			
 			for (int v=0 ; v<4 ; v++){
 				//cout << "(" << MarkersA[i][v].x << "," << MarkersA[i][v].y << ") - ";
-				getNearestPoint (cloudA, markers_cloudA, MarkersA[i][v].x, MarkersA[i][v].y);
+				//getNearestPoint (cloudA, markers_cloudA, MarkersA[i][v].x, MarkersA[i][v].y);
+
+				//markersA_struct[i].v1 = getNearestPoint (cloudA, MarkersA[i][v].x, MarkersA[i][v].y);
 			}
-			cout << endl;
-			MarkersA[i].draw(InImageA,Scalar(0,0,255),2);
+			//cout << endl;
+			MarkersA[i].draw(InImageA,cv::Scalar(0,0,255),2);
 		}
 		
 		// Stampa info su immagine B
 		std::cout << "\n*** MARKERS IMMAGINE B ***\n";
 		for(unsigned int i=0;i<MarkersB.size();i++){
-			cout << MarkersB[i].id << endl;
+			//cout << MarkersB[i].id << endl;
+			//markersB_struct[i].id = MarkersB[i].id;
+
 			for (int v=0 ; v<4 ; v++){
 				//cout << "(" << MarkersB[i][v].x << "," << MarkersB[i][v].y << ") - ";	
-				getNearestPoint (cloudB, markers_cloudB, MarkersB[i][v].x, MarkersB[i][v].y);
+				//getNearestPoint (cloudB, markers_cloudB, MarkersB[i][v].x, MarkersB[i][v].y);
+
+				//markersB_struct[i].v1 = getNearestPoint (cloudA, MarkersB[i][v].x, MarkersB[i][v].y);
 			}
-			cout << endl;
-			MarkersB[i].draw(InImageB,Scalar(0,0,255),2);
+			//cout << endl;
+			MarkersB[i].draw(InImageB,cv::Scalar(0,0,255),2);
 		}
 		
-
+		// Salvataggio delle due nuvole di marker
+		//pcl::io::savePCDFileASCII ("markers_cloudA.pcd", *markers_cloudA);
+		//pcl::io::savePCDFileASCII ("markers_cloudB.pcd", *markers_cloudB);
+		
 
 		//show input with augmented information
 		cv::imshow("inA",InImageA);
@@ -223,28 +485,88 @@ int main(int argc,char **argv) {
 		pass.setInputCloud (markers_cloudB);
 		pass.filter (*markers_cloudB);
 
-		/*
-		// PROVA DI ROTAZIONE DI markers_cloudB
-		Eigen::Matrix4f transformationMatrix;
-		float degree;
-		cout << "Angolo di rotazione della nuvola cloudB = ";
-		cin >> degree;
-		float angle = degree * M_PI / 180.0;
-		transformationMatrix << 
-			cos(angle),  -sin(angle), 1, 0, 
-			sin(angle),   cos(angle), 1, 0, 
-			         0,            0, 1, 0, 
-					 0,            0, 0, 1; 
+
+
+
+		// INIZIO PROCEDURA DI TRASFORMAZIONE MEDIANTE LE CORRISPONDENZE TROVATE
 		
-		pcl::transformPointCloud(*markers_cloudB,*rotatedCloud, transformationMatrix);
+		// Creazione dell'oggetto corrispondenze
+		pcl::TransformationFromCorrespondences correspondences;
+		// Creazione trasformazione SVD
+		pcl::registration::TransformationEstimationSVD <pcl::PointXYZRGBA, pcl::PointXYZRGBA> SVD;
+		// Creazione matrice di trasformazione
+		Eigen::Matrix4f transformation_matrix;
+
+
+		int idA = 0, idB = 0;
+		for (int i = 0; i < IDcomuni[i]; i++){
+			cout << "cerco per IDcomuni[" << i << "]" << endl;
+			if (MarkersA[idA].id == MarkersB[idB].id){
+				// trovata corrispondenza tra 2 ID --> aggiungo in ordine i punti dei 4 vertici alle corrispondenze
+				for (int v = 0; v < 4; v++) {
+					correspondences.add ((getNearestPoint3f(cloudA, MarkersA[idA][v].x, MarkersA[idA][v].y)), 
+														 getNearestPoint3f(cloudB, MarkersB[idB][v].x, MarkersB[idB][v].y), 1.0);
+					//cout << "Aggiunta corrispondenza #" << i << ": ID= " << MarkersA[idA].id << endl;
+					cout << "Aggiunta corrispondenza: ID = " << MarkersA[idA].id << " - Vertice " << v << endl;
+				}
+				// ho trovato una corrispondenza quindi scorro entrambi gli indici
+				idA++;
+				idB++;
+			} else {
+				// ho 2 ID diversi, guardo se è diverso quello di A o quello di B
+				if (IDcomuni[i] == MarkersA[idA].id){
+					//IDcomuni è corrispondente in A ma non in B
+					// scorro idB di una posizione e ritento la ricerca sulla stessa i
+					idB++;
+					i--;
+				}
+				else {
+					// IDcomuni è corrispondente in B ma non in A
+					// scorro idA di una posizione e ritento la ricerca sulla stessa i
+					idA++;
+					i--;
+				}
+			}
+		}
+
+		cout << "\nSono state aggiunte " << correspondences.getNoOfSamples() << " corrispondenze!\n";	
+
+
+
+
+		//transformation_matrix =	correspondences.getTransformation().matrix();
+		// Esecuzione del metodo che scriverà in transformation_matrix i valori per la trasformazione
+		//SVD.estimateRigidTransformation (&markers_cloudA, &markers_cloudB, correspondences, transformation_matrix);
+		//SVD.estimateRigidTransformationSVD (markers_cloudA, markers_cloudB, correspondences, transformation_matrix);
+		//cout << transformation_matrix << endl;
+		
+
+
+		//Eigen::Matrix4f trans; 
+		//pcl::registration::TransformationEstimationSVD<pcl::PointXYZRGBA,pcl::PointXYZRGBA> trans_est; 
+		//trans_est.estimateRigidTransformation(*markers_cloudA,*markers_cloudB,correspondences, trans); 
+
+		/*
+		pcl::PointCloud<pcl::PointXYZRGBA> cloud_out;// (new pcl::PointCloud<pcl::PointXYZRGBA>);	
+		pcl::transformPointCloud (*markers_cloudA, cloud_out, trans);
+
+		pcl::io::savePCDFileASCII("cloud_out.pcd", cloud_out);*/
+
+
+
+
+
+
+
+
+		/*
+		pcl::visualization::PCLVisualizer visCorr; 
+        //visCorr.addPointCloud(*markers_cloudA,"src"); 
+        visCorr.addPointCloud(cloud_out.makeShared(),"trgt"); 
+        visCorr.resetCamera(); 
+        visCorr.spin(); 
 		*/
 
-
-		// Salvataggio delle due nuvole di marker
-		pcl::io::savePCDFileASCII ("markers_cloudA.pcd", *markers_cloudA);
-		pcl::io::savePCDFileASCII ("markers_cloudB.pcd", *markers_cloudB);
-		//pcl::io::savePCDFileASCII ("rotated_markers_cloudB.pcd", *rotatedCloud);
-		
 		/*
 		pcl::visualization::CloudViewer viewer("viewer");
 		viewer.showCloud(cloudA);
